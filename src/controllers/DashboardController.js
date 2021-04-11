f047990530733787505719fee13e10388d3b13cc
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2021-04-10 21:35:38 -0300

    Adição de conteúdo da aula 03 (parte 03)
    Foi adicionado no DashboardController o reconhecimento da quantidade de Jobs (total, em progresso ou concluído), e as horas livres do dia.
    
    O index também está exibindo a foto de perfil e o nome salvos no profile.

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -6,13 +6,29 @@ module.exports = {
     index(request, response) {
         const Jobs = Job.get();
         const profile = Profile.get();
+
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: Jobs.length
+        }
+
+        // total de horas por dia de cada job em progresso
+        let jobTotalHours = 0
+
         const updatedJobs = Jobs.map((job) => {
             //  CALCULO DE TEMPO RESTANTE
             const remaining = JobUtils.remainingDays(job)
     
             // CALCULANDO STATUS
             const status = remaining <= 0 ? 'done' : 'progress' // If ternário
-    
+            
+            // SOMANDO A QUANTIDADE DE STATUS
+            statusCount[status] += 1;
+
+            // total de horas por dia de cada job em progresso
+            jobTotalHours = status == 'progress' ? jobTotalHours + Number(job['daily-hours']) : jobTotalHours
+
             // RETORNANDO VALORES NO JOB
             return {
                 ...job, // Pega tudo que tem no job já
@@ -22,6 +38,10 @@ module.exports = {
                 budget: JobUtils.calculateBudget(job, profile["value-hour"])
             }
         })
-        return response.render("index", { jobs: updatedJobs })
+
+        // qtd de horas que quero trabalhar - qtd de horas/dia de cada job em progress
+        const freeHours = profile["hours-per-day"] - jobTotalHours
+
+        return response.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours})
     }
 }
